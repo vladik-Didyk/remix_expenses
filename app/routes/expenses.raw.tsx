30239b18e2235da35d466999747956864c565518
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-15 14:49:25 -0400

    Introducing Resource Routes

--- app/routes/expenses.raw.tsx
+++ app/routes/expenses.raw.tsx
@@ -1,4 +1,20 @@
-export default function ExpensesRawPage(): JSX.Element {
-  console.log("ExpensesRawPage");
-  return <h1>Expenses Raw Page</h1>;
+// /expenses/raw -> resours link
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  {
+    id: "e2",
+    title: "Paper",
+    amount: 54.12,
+    date: new Date(2010, 7, 14),
+  },
+];
+
+export function loader() {
+  return DUMMY_EXPENSES;
 }
