636cdc520a2376c03a3a372cf0c49f18b14d2920
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-18 07:35:11 -0400

    69. Loaders - A Closer Look

--- app/components/expenses/ExpensesList.tsx
+++ app/components/expenses/ExpensesList.tsx
@@ -3,15 +3,16 @@ import ExpenseListItem from "./ExpenseListItem.js";
 function ExpensesList({ expenses }) {
   return (
     <ol id="expenses-list">
-      {expenses.map((expense) => (
-        <li key={expense.id}>
-          <ExpenseListItem
-            id={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-          />
-        </li>
-      ))}
+      {expenses &&
+        expenses.map((expense) => (
+          <li key={expense.id}>
+            <ExpenseListItem
+              id={expense.id}
+              title={expense.title}
+              amount={expense.amount}
+            />
+          </li>
+        ))}
     </ol>
   );
 }
