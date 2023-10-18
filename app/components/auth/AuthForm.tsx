9cf08a175a899d2247eb1ce387fce2c78ecab983
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-18 13:09:43 -0400

    91. Adding Server-side Credentials Validation

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,4 +1,10 @@
-import { Form, Link, useNavigation, useSearchParams } from "@remix-run/react";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "@remix-run/react";
 
 // import { FaLock } from 'react-icons/fa';
 
@@ -6,6 +12,7 @@ function AuthForm() {
   const navigation = useNavigation();
 
   const [searchParams] = useSearchParams();
+  const validationErrors = useActionData();
   const authMode = searchParams.get("mode") || "login";
   const submitBtnCaption = authMode === "login" ? "Login" : "Create User";
   const toggleBtnCaption =
@@ -23,6 +30,13 @@ function AuthForm() {
         <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" minLength={7} />
       </p>
+      {validationErrors && (
+        <ul>
+          {Object.values(validationErrors).map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <div className="form-actions">
         <button disabled={isSubmitting}>
           {isSubmitting ? "Authenticating..." : submitBtnCaption}
--- app/routes/_marketing.auth.tsx
+++ app/routes/_marketing.auth.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunction } from "@remix-run/react";
 import expensesAuth from "~/styles/auth.css";
 import AuthForm from "~/components/auth/AuthForm";
+import { validateCredentials } from "~/data/validation.server";
 
 export const links = () => {
   return [{ rel: "stylesheet", href: expensesAuth }];
@@ -13,12 +14,20 @@ export async function action({ request }) {
   const formData = await request.formData();
   const credentials = Object.fromEntries(formData);
 
+  // validate user input
+
+  try {
+    validateCredentials(credentials);
+  } catch (error) {
+    return error;
+  }
   if (authMode === "login") {
     // login logic
   } else {
     // signup logic
   }
 }
+
 export const loader: LoaderFunction = async () => {
   return {};
 };
