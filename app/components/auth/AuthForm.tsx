cc7dc17a4291c677c286e4f037bb995a6c4a2c71
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-18 11:48:14 -0400

    78. Adding Visual Feedback to the Authentication Form

--- app/components/auth/AuthForm.tsx
+++ app/components/auth/AuthForm.tsx
@@ -1,14 +1,17 @@
-import { Link, useSearchParams, Form } from "@remix-run/react";
+import { Form, Link, useNavigation, useSearchParams } from "@remix-run/react";
 
 // import { FaLock } from 'react-icons/fa';
 
 function AuthForm() {
+  const navigation = useNavigation();
+
   const [searchParams] = useSearchParams();
   const authMode = searchParams.get("mode") || "login";
   const submitBtnCaption = authMode === "login" ? "Login" : "Create User";
   const toggleBtnCaption =
     authMode === "login" ? "Create a new user" : "Login with existing user";
 
+  const isSubmitting = navigation.state !== "idle";
   return (
     <Form method="post" className="form" id="auth-form">
       <div className="icon-img">{/*<FaLock />*/}</div>
@@ -21,7 +24,9 @@ function AuthForm() {
         <input type="password" id="password" name="password" minLength={7} />
       </p>
       <div className="form-actions">
-        <button>{submitBtnCaption}</button>
+        <button disabled={isSubmitting}>
+          {isSubmitting ? "Authenticating..." : submitBtnCaption}
+        </button>
         <Link to={authMode === "login" ? "?mode=signup" : "?mode=login"}>
           {toggleBtnCaption}
         </Link>
