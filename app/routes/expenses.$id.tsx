088de82ec097be21b6f282392372d74facc85407
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-18 11:05:43 -0400

    73. Updating Data

--- app/routes/expenses.$id.tsx
+++ app/routes/expenses.$id.tsx
@@ -2,9 +2,26 @@
 
 import type { LoaderFunction } from "@remix-run/react";
 import { useNavigate } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
-import { getExpense } from "~/data/expenses.server";
+import { getExpense, updateExpense } from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
+
+export async function action({ params, request }) {
+  const expenseId = params.id;
+  const formData = await request.formData();
+  const expenseData = Object.fromEntries(formData);
+
+  try {
+    validateExpenseInput(expenseData);
+  } catch (error) {
+    return error;
+  }
+
+  await updateExpense(expenseId, expenseData);
+  return redirect("/expenses");
+}
 
 export async function loader({ params }): LoaderFunction {
   const expenseId = params.id;
