aa81328ef66e3f1f7c1d229abc18170c8c918d06
vladik-Didyk <62429920+vladik-Didyk@users.noreply.github.com>
2023-10-17 23:46:24 -0400

    68. Fetching Expenses (via loader())

--- app/routes/expenses._index.tsx
+++ app/routes/expenses._index.tsx
@@ -1,28 +1,16 @@
 import type { LoaderFunction } from "@remix-run/react";
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import ExpensesList from "~/components/expenses/ExpensesList";
+import { getExpenses } from "~/data/expenses.server";
 // import {FaPlus, FaDownload } from "react-icons/fa";
 
-export const loader: LoaderFunction = async () => {
-  return {};
-};
+export function loader(): LoaderFunction {
+  return getExpenses();
+}
 
-const DUMMY_EXPENSES = [
-  {
-    id: "e1",
-    title: "Toilet",
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-  },
-  {
-    id: "e2",
-    title: "Paper",
-    amount: 54.12,
-    date: new Date(2010, 7, 14),
-  },
-];
 export default function ExpensesLayout(): JSX.Element {
-  console.log("ExpensesPage");
+  const expenses = useLoaderData();
+
   return (
     <>
       <Outlet /> {/* This will render the nested route */}
@@ -37,7 +25,7 @@ export default function ExpensesLayout(): JSX.Element {
             <span>Load Raw Data</span>
           </a>
         </section>
-        <ExpensesList expenses={DUMMY_EXPENSES} />
+        <ExpensesList expenses={expenses} />
       </main>
     </>
   );
